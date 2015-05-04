5765369fe549278f3ef482504699f0e88af75376
MarcN04 <marc-niedermeier@web.de>
2015-05-04 22:58:11 +0200

    Start
    Hoffentlich kriege ich den scheiß Graphen hin..

---
+++ MaiPi/app/src/main/java/com/rocks/maipi/maipi/DisplayActivityListener.java
@@ -0,0 +1,28 @@
+package com.rocks.maipi.maipi;
+
+import android.content.DialogInterface;
+import android.view.View;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by Marc on 03.05.2015.
+ */
+public class DisplayActivityListener implements View.OnClickListener{
+
+    DisplayActivity mActivity;
+    DisplayActivityGUI mGUI;
+
+    DisplayActivityListener(DisplayActivity activity, DisplayActivityGUI gui){
+        mActivity = activity;
+        mGUI = gui;
+        gui.getJSONButton().setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        JSONTask loader = new JSONTask(mActivity, mGUI);
+        loader.execute();
+    }
+}
---
+++ MaiPi/app/src/main/java/com/rocks/maipi/maipi/JSONTask.java
@@ -0,0 +1,51 @@
+package com.rocks.maipi.maipi;
+
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.widget.ListAdapter;
+import android.widget.SimpleAdapter;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by Marc on 04.05.2015.
+ */
+public class JSONTask extends AsyncTask <Void, Void, JSONObject> {
+
+    private DisplayActivity mActivity;
+    private DisplayActivityGUI mGUI;
+
+    public JSONTask (DisplayActivity activity, DisplayActivityGUI gui){
+        mActivity = activity;
+        mGUI = gui;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        // Showing progress dialog
+        mGUI.getJSONText().setText("JSON wird geladen!");
+    }
+
+    @Override
+    protected JSONObject doInBackground(Void... params) {
+        String url = "http://192.168.2.121:3000/temp";
+
+        JSONParser jParser = new JSONParser();
+        JSONObject json = jParser.getJSONFromUrl(url);
+
+        return json;
+    }
+
+    @Override
+    protected void onPostExecute(JSONObject result) {
+        super.onPostExecute(result);
+        // Dismiss the progress dialog
+        try {
+            mGUI.getJSONText().setText(result.getString("name") + result.getString("temp"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+}
